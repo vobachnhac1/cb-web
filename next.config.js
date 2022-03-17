3eb27da8ccb11f8a5b716ce0853f267972d9efbb
nhac.vo <nhac.vo@dounets.com>
2022-03-17 23:26:37 +0700

    fix build production

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 
-const withAntdLess = require("next-plugin-antd-less");
+// const withAntdLess = require('next-plugin-antd-less');
+import withAntdLess from 'next-plugin-antd-less';
 module.exports = {
   // i18n,
   eslint: {
@@ -10,9 +11,9 @@ module.exports = {
 module.exports = withAntdLess({
   reactStrictMode: true,
   // You can directly change the antd less variables here
-  // modifyVars: { "@primary-color": "#f200ff" },
+  // modifyVars: { '@primary-color': '#f200ff' },
   // Or better still you can specify a path to a file
-  lessVarsFilePath: "./src/styles/variables.less",
+  lessVarsFilePath: './src/styles/variables.less',
   // optional
   lessVarsFilePathAppendToEndOfContent: false,
   // optional https://github.com/webpack-contrib/css-loader#object
