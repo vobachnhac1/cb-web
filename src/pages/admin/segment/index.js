57f73d78402557c0295bc5166126e603c71528ca
quynam <quynamle123@gmail.com>
2022-04-09 14:21:32 +0700

    update segment

--- src/pages/admin/segment/index.js
+++ src/pages/admin/segment/index.js
@@ -5,98 +5,150 @@
 * Created: 2022-04-07
 *------------------------------------------------------- */
 require("./style.module.less");
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import * as styles from './style.module.less';
 import * as classnames from 'classnames';
 import LayoutHome from '@/containers/Home';
-import { Button, Card, Col, Row, Space, Table,Popconfirm } from 'antd';
+import { Button, Card, Col, Row, Space, Table, Popconfirm, Select, Typography, Input } from 'antd';
+const { Text } = Typography;
+import * as Message from '@/components/message';
 import ModalSegment from '@/containers/modal-segment';
 import ModalTopic from '@/containers/modal-topic';
 // khai báo store
 import { useSelector, useDispatch } from 'react-redux';
 import { actions as actionSegment } from '@/redux/segment';
 import { getters as gettersSegment } from '@/redux/segment';
+import { actions as actionTopic } from '@/redux/topic';
+import { getters as gettersTopic } from '@/redux/topic';
 
+import moment from 'moment';
 // handhandleDelete
 
 
 export default function Segment(props) {
+  const [topicId, setTopicId] = useState('');
+  const [dataSearch, setDataSearch] = useState('')
   const dispatch = useDispatch();
- const listSegment = useSelector(gettersSegment.getStateLoadPageSegment) || [];
-
+  const listSegment = useSelector(gettersSegment.getStateLoadPageSegment) || [];
+  const listTopic = useSelector(gettersTopic.getStateLoadPageTopic) || [];
 
   // gọi 1 function rồi theo dõi nhưng thay đổi của param đó
   useEffect(() => {
     initPage(); // chjay 1 lần duy nhất
   }, [])
-   useEffect(() => {
-    
-  }, [listSegment])
 
-  // useEffect(() => {
-  //   // chạy khi có sụ thay đổi của listTopic
-  // }, [listSegment])
 
   const initPage = async () => {
-    await dispatch(actionSegment.searchSegment()); 
+    const paramsInitSegment = {
+      "topic_id": 0,
+      "segment_id": 0,
+      "segment_name": "string",
+      "segment_color": "string",
+      "inactived_date": "2022-04-08T04:17:56.025Z",
+      "created_date": "2022-04-08T04:17:56.025Z",
+      "datelastmaint": "2022-04-08T04:17:56.025Z",
+      "is_approve": true
+    }
+    await dispatch(actionSegment.searchSegment(paramsInitSegment));
+    await dispatch(actionTopic.searchTopic());
+  }
+
+  const searchBtn = async () => {
+    let paramsSearch = {
+      "topic_id": topicId,
+      "segment_id": 0,
+      "segment_name": "string",
+      "segment_color": "string",
+      "inactived_date": "2022-04-08T04:17:56.025Z",
+      "created_date": "2022-04-08T04:17:56.025Z",
+      "datelastmaint": "2022-04-08T04:17:56.025Z",
+      "is_approve": true,
+      'dataSearch': dataSearch,
+    }
+    console.log(paramsSearch)
+    await dispatch(actionSegment.searchSegment(paramsSearch));
   }
 
 
-  const handleDelete = (key) => {
-    console.log('đã click nut delete')
-};
-const columns = [
-  {
-    title: 'Mã kết quả trúng thưởng',
-    dataIndex: 'segment_id',
-    key: 'segment_id',
-    fixed: 'left'
-    // render: text => <a>{text}</a>,
-  },
-  {
-    title: 'Tên kết quả trúng thưởng',
-    dataIndex: 'segment_name',
-    key: 'segment_name',
-    fixed: 'left'
-  },
-  {
-    title: 'Chủ đề',
-    dataIndex: 'topic_name',
-    key: 'topic_name',
-    
-   
-  },
-  {
-    title: 'Ngày hết hiệu lực',
-    dataIndex: 'inactived_date',
-    key: 'inactived_date',
-  },
-   {
-    title: 'Ngày tạo',
-    dataIndex: 'created_date',
-    key: 'created_date',
-  }, 
-  
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) =>(
+  const handleDelete = async (record) => {
+    let dataRecord = { ...record }
+    console.log('đã click nut delete', dataRecord)
+    const result = await dispatch(actionSegment.deleteSegmentById(dataRecord));
+    if (result) {
+      initPage();
+      Message.Success("NOTYFICATON", "DELETE TOPIC SUCCESS");
+      return
+    }
+    Message.Error("NOTYFICATON", "DELETE TOPIC FAIL");
+  };
+  const columns = [
+    {
+      title: 'Mã kết quả\n trúng thưởng',
+      dataIndex: 'segment_id',
+      key: 'segment_id',
+      fixed: 'left',
+      width: 100
+      // render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Tên kết quả \n trúng thưởng',
+      dataIndex: 'segment_name',
+      key: 'segment_name',
+      fixed: 'left',
+      width: 250
+    },
+    {
+      title: 'Chủ đề',
+      dataIndex: 'topic_name',
+      key: 'topic_name',
+      fixed: 'center',
+      width: 250,
+
+    },
+    {
+      title: 'Ngày hết hiệu lực',
+      dataIndex: 'inactived_date',
+      key: 'inactived_date',
+      width: 170,
+      render: (text, record) => {
+        return <p>
+          {!text ? '' : moment(text).format('YYYY-MM-DD, hh:mm:ss')}
+        </p>
+      }
+    },
+    {
+      title: 'Ngày tạo',
+      dataIndex: 'created_date',
+      key: 'created_date',
+      width: 170,
+      render: (text, record) => {
+        return <p>
+          {moment(text).format('YYYY-MM-DD, hh:mm:ss')}
+        </p>
+      }
+    },
+
+    {
+      title: 'Action',
+      key: 'action',
+      width: 140,
+      render: (text, record) => (
 
         <Space size="middle">
-        <Button style={{ color: 'blue', borderColor: 'blue', borderWidth: 0.5 }} onClick={() => updateSegment(record)} >Edit</Button>
-      
-        { listSegment.length >= 1 ? (
-              <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)} >
-                <Button style={{ color: 'red', borderColor: 'red', borderWidth: 0.5 }}>Delete</Button>
-              </Popconfirm>
-            ) : null
+          <Button style={{ color: 'blue', borderColor: 'blue', borderWidth: 0.5 }} onClick={() => updateSegment(record)} >Edit</Button>
+
+          {listSegment.length >= 1 ? (
+            <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record)} >
+              <Button style={{ color: 'red', borderColor: 'red', borderWidth: 0.5 }} >Delete</Button>
+            </Popconfirm>
+          ) : null
           }
-      </Space>
-     ),
-  },
-];
+        </Space>
+      ),
+    },
+  ];
   const pagination = {
     current: 1,
     pageSize: 10,
@@ -104,7 +156,7 @@ const columns = [
 
   };
 
-  
+
   const [visible, setVisible] = useState(false);
   const [bodyModel, setBodyModel] = useState({
     isAdd: false,
@@ -126,15 +178,6 @@ const columns = [
     });
   }
 
-  const deleteTopic = async (record) => {
-    const result = await dispatch(actionTopic.deleteTopic(record));
-    if (result) {
-      initPage();
-      Message.Success("NOTYFICATON", "DELETE TOPIC SUCCESS");
-      return
-    }
-    Message.Error("NOTYFICATON", "DELETE TOPIC FAIL");
-  }
 
   const callbackModal = (params) => {
     setVisible(params.visible);
@@ -150,17 +193,35 @@ const columns = [
           headStyle={{ fontSize: 20, color: 'rgba(255, 255, 255, 1)', fontWeight: 'bold', textAlign: 'center', backgroundColor: "rgb(3, 77, 162)" }}
           title="Tất cả kết quả giải thưởng"
           bordered={true}
-          style={{ backgroundColor: '#FFFFFF',padding:0 }}>
+          style={{ backgroundColor: '#FFFFFF', padding: 0 }}>
           <Col span={48}>
             <Row gutter={[16, 24]}>
+
+              <Col className="gutter-row"  >
+                <Text style={{ marginLeft: '4px' }}>{'Chủ đề :'}</Text>
+              </Col>
+              <Col className="gutter-row" span={4}>
+
+                <Select
+                  style={{ width: '100%' }}
+                  defaultValue=""
+                  value={topicId}
+                  onChange={(value) => setTopicId(value)}>
+                  {listTopic.map((Item, key) => (
+                    <Select.Option value={Item.topic_id} key={key}>{Item.topic_name}</Select.Option>
+                  ))}
+                </Select>
+              </Col>
+              <Col className="gutter-row" span={12}>
+                <Input placeholder="Thông tin cần tìm" onChange={(event) => setDataSearch(event.target.value)} />
+              </Col>
+            </Row>
+            <Row gutter={[16, 24]} style={{ marginTop: '10px' }}>
               <Col className="gutter-row" span={3}>
-               
-								<Button type='primary' size='middle' style={{ width: '100%' }} onClick={addNewSegment}>Thêm</Button>
-						
-                
+                <Button type='primary' size='middle' style={{ width: '100%' }} onClick={addNewSegment}>Thêm</Button>
               </Col>
               <Col className="gutter-row" span={3}>
-                <Button type='primary' size='middle' style={{ width: '100%' }}>Tìm kiếm</Button>
+                <Button type='primary' size='middle' style={{ width: '100%' }} onClick={searchBtn}>Tìm kiếm</Button>
               </Col>
             </Row>
           </Col>
--- src/pages/admin/topic/index.js
+++ src/pages/admin/topic/index.js
@@ -14,7 +14,8 @@ import * as Message from '@/components/message';
 
 // khai báo store
 import { useSelector, useDispatch } from 'react-redux';
-import { actions as actionTopic } from '@/redux/topic';
+import { actions as actionTopic } from 
+'@/redux/topic';
 import { getters as gettersTopic } from '@/redux/topic';
 import ModalTopic from '@/containers/modal-topic';
 
